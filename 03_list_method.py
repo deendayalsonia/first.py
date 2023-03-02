defdbfc0a7f44cd892a15294899608cb11a3214a
deendayalsonia <126788115+deendayalsonia@users.noreply.github.com>
2023-03-02 05:38:47 -0800

    Add files via upload

---
+++ 03_list_method.py
@@ -0,0 +1,17 @@
+# list method
+l1 = [ 1, 23, 10, 50,9]
+# l1.sort()
+# print(l1)
+# l1.reverse()
+# print(l1)
+# l1.append(45) add 45 to the list
+print(l1)
+
+# l1.insert(1, 234)
+# print(l1) insert 234 at index 1
+
+# l1.pop(2) #pop the value at index 2
+# print(l1)
+l1.remove(23)
+print(l1)
+
